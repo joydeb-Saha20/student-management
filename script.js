bbfb4c65c029048c196ea8e279a7db2da3e1091a
joydeb-Saha20 <jsaha8834@gmail.com>
2026-07-20 21:14:52 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function showForm(formId){
-
-    document.querySelectorAll(".login-container, .container").forEach(form => form.classList.remove("active"));
-
-    document.getElementById(formId).classList.add("active");
-
-}
-
